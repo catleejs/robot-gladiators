54451a0e78b187a355acd34c1ce71e7e957d09f2
unknown <madileesmith@gmail.com>
2021-08-17 22:42:40 -0500

    began console commands

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+var playerName = window.prompt("What is your robot's name?");
+var name = "your name";
+
+console.log(name);
+console.log(playerName);
+console.log("This log is a string, good for leaving a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+// 3.1.6
